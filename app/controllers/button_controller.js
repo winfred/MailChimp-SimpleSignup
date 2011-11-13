e5b24d935dacc67005990c8073de081207fd9c10
Winfred <winfred.nadeau@gmail.com>
2011-11-13 01:03:01 -0500

    customizations: optin-type, bg-color, text, width, font-size, scrolling text toggle

--- app/controllers/button_controller.js
+++ app/controllers/button_controller.js
@@ -1,9 +1,7 @@
 module.exports.show = function(req,res){
    res.render('button/show',{
        layout: false,
-      user_id: req.query.u,
-      list_id: req.query.id,
-      signup_prompt: req.query.signup_prompt
+		options: req.query
    }); 
 };
 
