913d94322a2790995545e7ca28a0096d85f55431
Winfred <winfred.nadeau@gmail.com>
2011-11-13 01:04:04 -0500

    Revert "customizations: optin-type, bg-color, text, width, font-size, scrolling text toggle"
    This reverts commit e5b24d935dacc67005990c8073de081207fd9c10.

--- app/controllers/button_controller.js
+++ app/controllers/button_controller.js
@@ -1,7 +1,9 @@
 module.exports.show = function(req,res){
    res.render('button/show',{
        layout: false,
-		options: req.query
+      user_id: req.query.u,
+      list_id: req.query.id,
+      signup_prompt: req.query.signup_prompt
    }); 
 };
 
