2db1b2a67232a506c18c84a942cbdec51aa14eaa
Winfred <winfred.nadeau@gmail.com>
2011-11-30 23:45:58 -0500

    adding logging console script
    $ node
    c = require('./app/logging_console')
    c.findAll();

---
+++ app/logging_console.js
@@ -0,0 +1,11 @@
+module.exports.mongoose = require('mongoose');
+module.exports.server = require('./server');
+require('./models/log');
+var Log = mongoose.model('LogEntry');
+module.exports.Log = Log;
+module.exports.findAll = function(cb){
+	Log.find({},function(err,docs){
+		console.log(docs);
+		cb ? cb(docs) : '';
+	})
+}
