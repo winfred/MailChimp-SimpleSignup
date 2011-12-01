9f2c19abfe8a082086b6c67dc8383e959be5688e
Winfred <winfred.nadeau@gmail.com>
2011-11-30 22:21:18 -0500

    another typo in button_controller, time to sleep

--- app/controllers/button_controller.js
+++ app/controllers/button_controller.js
@@ -1,6 +1,5 @@
 var Logger = mongoose.model('LogEntry');
 module.exports.show = function(req,res){
-	if (req.)
 	Logger.registerView(req,function(){
 		res.render('button/show',{
 		       layout: false,
